cf48174892f8dab6aa943feb4132769e67156979
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2024-02-01 22:54:32 +0530

    :rocket: init: drf swagger (cpu-metrics)

---
+++ cpu-metrics/cpu_metrics/api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from rest_framework import permissions  
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from .views import * 
+
+get_schema_view = get_schema_view(
+    openapi.Info(
+        title="CPU Metrics API",
+        default_version="v1",
+        description="CPU Metrics API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="aditya.pai@somaiya.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    # swagger and redoc
+    path("", get_schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("docs/", get_schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
+    # api
+    path('hello-world/', hello_world, name='hello_world')
+]
---
+++ cpu-metrics/cpu_metrics/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.decorators import api_view  
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+@api_view(["GET"])
+def hello_world(request: Request) -> Response:
+    return Response({"message": "Hello, world!"})  
